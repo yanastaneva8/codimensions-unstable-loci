56c30d5fa937fbc63cf97fea91b219e22466f006
yanastaneva8 <yanastaneva8@gmail.com>
2020-08-26 15:52:13 +0300

    modified A_n case

--- A_n.py
+++ A_n.py
@@ -14,16 +14,21 @@ We are interested in the description of the GIT-classes of ample line bundles on
 This script computes the extremal values for codimensions of unstable loci.
 SL_2-subgroups of SL_n
 Type A_n
+
+Python 3.7
 """
 
 
+# Import necessary python packages, if unavailable, use command - pip3 install numpy
 import numpy as np
+# Import necessary python packages, if unavailable, use command - pip3 install itertools
 import itertools
 from numpy.linalg import inv
 
-# initialize a list to store the computations for the main loop
+
+# Initialize a list to store the computations for the main loop
 comp_list = []
-# defines a class to store all computations
+# Defines a class to store all computations
 class Computations: 
     def __init__(self, binary, h, length, weyl, scalar_product):
         self.binary = binary
@@ -39,121 +44,151 @@ class Computations:
         return getattr(self, item)
 
 
-# defines a function to generate the fundamental weights b
+# Defines a function to generate the fundamental weights b
 def binary_a(n):
+    # Initialize an empty nx1 array 
     b = np.zeros(n)
-    binary = np.ones(shape=(n,n))
-    for i in range(n):
+    # Initialize an identity nxn array  
+    binary = np.zeros(shape=(n,n))
+    # Iterate over all values up to n
+    for i in range(0,n):
+        # Insert a 1 in the i-th entry of the nx1 vector b
         np.put(b, i, 1)
-        binary[i]=b
+        # Insert the resulting b vector in the initialized array to store
+        binary[i]=b   
     return binary
 
 
-# defines a function to generate the vectors h
+# Defines a function to generate the vectors h
 def h_vector(n):
+    ## Name of M?
+    # Generate the nxn matrix M, s.t. h = Mv, v is a vector with entries 1 & 0
     M = np.triu(2*np.ones(n))
     M[:, -1] = np.ones(n)
-    3# print('M = %s' %M)
+    # print('M = %s' %M)
+    # Call function for fundamental weights b to obtain all possible vectors v s.t. h = Mv
     b = binary_a(n)
+    # Initialize empty list to store the resulting vectors h
     h_array=[]
+    # Iterate over all possible fundamental weights b
     for i in range(len(binary_a(n))):
+        # Compute the dot product h = Mv for v=b_i
         h = np.dot(M, np.array([b[i]]).T)
+        # Store resulting vector h in the list h_array
         h_array.append(np.asarray(h))
         # print('h_array = %s' % np.ravel(h)   ) 
     return h_array
 
-# defines a function to generate the permutation in matrix form w.r.t. the basis elements of the group
+
+# Defines a function to generate the permutation in matrix form w.r.t. the simple reflections of the group
 def matrix_form(elt):
+    # print(elt)
+    # Assign the symbolic expression of the simple reflections, e.g. r_1,...,r_n, r_1*r_2,...,r_1*r_n, ...
+    # and create a list which stores the symbolic expression generated above  
     elts = list(elt)
+    # print(elts)
+    # Store only the indeces of the generated reflections, i.e. if w=r_1*r_2*r_3, only store 123
     reflections = elts[1::3]
+    # print(reflections)
+    # Initialize empty nxn identity matrix
     matrixform = np.eye(n)
+    # Loop to iterate over all generated reflection expressions
     for elt in reflections:
+        # Assign the index of the current reflection to the variable a
         a = int(elt)
+        # Initialize empty nxn identity matrix to generate the basis elements
         mat = np.eye(n)
+        # The n-th simple reflection element with -1 in the (n,n) position
         if a == n:
             mat[a-1, a-1] = -1
+            # Compute the matrix form for the n-th simple reflection
             matrixform = np.dot(matrixform, mat)
+        # The rest j=1,...,n-1 simple reflections for type A_n, the block ((0,1),(1,0)) shifted along the diagonal to begin at j-th row    
         else:
-            #mat[a - 2, a - 2] = 1
             mat[a-1, a] = 1
             mat[a, a - 1] = 1
             mat[a,a] = 0
             mat[a-1, a-1] = 0
+            # Compute the matrix form for the j-th simple reflection
             matrixform = np.dot(matrixform, mat)
     return matrixform
 
 
-# initialize two lists, one for the generating expressions of the permutations, one for the the list of expressions
-# when needed, comment in or out the lines which generate the n-tuples of permutations
+# Initialize two lists, one for the generating symbolic reflections (r_1,r_2,...,r_j,...,r_n), 
+# one for the tuples of reflections, i.e. w = r_1*r_2*...*r_j.
+# To reduce complexity, comment in or out the lines which generate the n-tuples of reflections needed,
+# i.e. if you want to test length l=3, then leave uncommented lines 132-133
 generators = []
 all_possible_roots = []
 def generate_reflections(n):
     for i in range(1, n+1):
         generators.append("r"+str(i))
-    # the line below generates expressions with length 1
+    # The line below generates expressions with length 1
     all_possible_roots.extend(generators)   
-    # the line below generates expressions with length 2
+    # The line below generates expressions with length 2
     for subset in itertools.product(generators, generators):
         all_possible_roots.append('*'.join(subset)) 
-    # the line below generates expressions with length 3
+    # The line below generates expressions with length 3
     for subset in itertools.product(generators, generators, generators):
         all_possible_roots.append('*'.join(subset))
-    # the line below generates expressions with length 4
+    # The line below generates expressions with length 4
     for subset in itertools.product(generators, generators, generators, generators):
         all_possible_roots.append('*'.join(subset))
-    # the line below generates expressions with length 5
+    # The line below generates expressions with length 5
 #     for subset in itertools.product(generators, generators, generators, generators, generators):
 #         all_possible_roots.append('*'.join(subset))
-    # the line below generates expressions with length 6
+    # The line below generates expressions with length 6
 #     for subset in itertools.product(generators, generators, generators, generators, generators, generators):
 #         all_possible_roots.append('*'.join(subset))
-    # the line below generates expressions with length 7
+    # The line below generates expressions with length 7
 #     for subset in itertools.product(generators, generators, generators, generators, generators, generators, generators):
 #         all_possible_roots.append('*'.join(subset))
-    # the line below generates expressions with length 8
+    # The line below generates expressions with length 8
 #     for subset in itertools.product(generators, generators, generators, generators, generators, generators, generators, generators):
 #         all_possible_roots.append('*'.join(subset))
-    # the line below generates expressions with length 9
+    # The line below generates expressions with length 9
 #     for subset in itertools.product(generators, generators, generators, generators, generators, generators, generators, generators, generators):
 #         all_possible_roots.append('*'.join(subset))
-    # the line below generates expressions with length 10
+    # The line below generates expressions with length 10
 #     for subset in itertools.product(generators, generators, generators, generators, generators, generators, generators, generators, generators, generators):
 #         all_possible_roots.append('*'.join(subset))
     return(all_possible_roots)
 
-# input here the dimension
+
+# Input here the dimension
 n = 3
-# call function to generate the vectors h
+# print('dimension n = %s' %n)
+# Call function to generate the vectors h
 h_store = h_vector(n)
-#print('h_store = %s' %h_store)
-# call function to generate fundamental weights b
+# print('array with h vectors, h_store = %s' %h_store)
+# Call function to generate fundamental weights b
 binary = binary_a(n)
-#print('b = %s' %binary)
-# call function to generate reflections r
+# print('fundamental weights, b_i = %s' %binary)
+# Call function to generate reflections w
 generators = generate_reflections(n)
-#print('reflection = %s' %generators)
-# initialize list to store the reflections which yield a nonzero scalar product
+# print('reflection, symbolic_w = %s' %generators)
+# Initialize list to store the reflections which yield a nonzero scalar product
 nonzero_reflections = [] 
-# initialize list to store the reflections in matrix form which yield a nonzero scalar product
+# Initialize list to store the reflections in matrix form which yield a nonzero scalar product
 nonzero_reflections_matrix_form = []
-# initialize a list to store the resulting scalar products
+# Initialize a list to store the resulting scalar products
 scalar = []
 # Main loop to go over all permutations generated
 for i in generators:
-    #print('refl = %s' %i)
+    # print('reflection symbolic expression, w = %s' %i)
     # Convert the generated reflection into matrix form
     a = matrix_form(i)
-    #print('reflmatrix = %s' %a)
+    # print('reflection in matrix form, w = %s' %a)
     # Store the current reflection
     nonzero_reflections.append(i)
     # Store the current reflection in matrix form
     nonzero_reflections_matrix_form.append(a)
-    # Nested loop to go over the list of fundamental weights
+    # Nested loop to iterate all fundamental weights b
     for j in range(len(binary)):
-        #print('b = %s' %binary[j])
+        # print('fundamental weight, b = %s' %binary[j])
         # Apply the generated permutation on the fundamental weight in the current loop
         weyl_acting_b = np.dot(a, binary[j])
-        #print('weyl = %s' %weyl_acting_b)
+        # print('reflection applied to binary vector, wb = %s' %weyl_acting_b)
         # Nested loop to go over all generated h vectors
         for h in h_store:
             # Compute the scalar product of the applied permutation onto the fundamental weight, wb, and the vector h
@@ -183,22 +218,20 @@ for i in generators:
 for item in comp_list:
     print(item)
 
-    
-# [1 1 , ... 0] implement 
-
-
-# In[ ]:
 
 
 """ Script to convert the resulting computations into a LaTeX table
 """
 
 
+# Import necessary python packages, if unavailable, use command - pip3 install pandas
 import pandas as pd
+# Import necessary python packages, if unavailable, use command - pip3 install tabulate
 import tabulate
 from tabulate import tabulate
 
 
+# Defines a function to print output from class Computations in ready to paste LaTeX table format
 def latex_with_lines(df, *args, **kwargs):
     kwargs['column_format'] = '|'.join([''] + ['l'] * df.index.nlevels
                                             + ['r'] * df.shape[1] + [''])
@@ -206,6 +239,8 @@ def latex_with_lines(df, *args, **kwargs):
     res = df.to_latex(*args, **kwargs)
     return res.replace('\\\\\n', '\\\\ \\hline\n')
 
+
+# Call function to print output in ready to paste in LaTeX table format
 df = pd.DataFrame({'Ah': x.h.astype(int), 'Binary': x.binary.astype(int), 'Length': x.length, 'Scalar': x.scalar_product, 'CWeyl': x.weyl} for x in comp_list)
 df_to_print = latex_with_lines(df)
 print(df_to_print)
